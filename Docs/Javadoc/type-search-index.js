f7c6b87aeb2c06c3e54cd701ebc1984539321284
matusalik <mateuszsalik@gmail.com>
2026-05-06 23:04:36 +0200

    Added javadoc documentation for projects services.

---
+++ Docs/Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.sii.Services","l":"GymService"},{"p":"org.sii.Services","l":"MemberService"},{"p":"org.sii.Services","l":"MembershipService"}];updateSearchResults();
